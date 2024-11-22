299c3cc489dcfecc624c5deb0404c7f6d5da053f
Dickens Au <dickens721@gmail.com>
2024-11-22 16:11:45 +0800

    2024-11-22

--- js/data/slide_hongkong_data.js
+++ js/data/slide_hongkong_data.js
@@ -1,7 +1,7 @@
 const slideHongKongData = [
-  "../../img/hongkong/hongkong_slide_002.jpg",
-  "../../img/hongkong/hongkong_slide_004.jpg",
-  "../../img/hongkong/hongkong_slide_005.jpg",
+  "/img/hongkong/hongkong_slide_002.jpg",
+  "/img/hongkong/hongkong_slide_004.jpg",
+  "/img/hongkong/hongkong_slide_005.jpg",
 ];
 
 export default slideHongKongData;
