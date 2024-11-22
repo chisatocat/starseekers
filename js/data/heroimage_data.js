f32aadb316842e6153ebe4a9c51645b991bdb917
Dickens Au <dickens721@gmail.com>
2024-11-22 17:17:40 +0800

    2024-11-22 ver 1.04

--- js/data/heroimage_data.js
+++ js/data/heroimage_data.js
@@ -1,7 +1,15 @@
 const heroImages = [
-  { src: "./img/iceland_v.jpg", alt: "冰島", link: "./starseekers_iceland.html" },
+  {
+    src: "./img/iceland_v.jpg",
+    alt: "冰島",
+    link: "./starseekers_iceland.html",
+  },
   { src: "./img/japan_v.jpg", alt: "日本", link: "./starseekers_japan.html" },
-  { src: "./img/uluru_v.jpg", alt: "香港", link: "./starseekers_hongkong.html" },
+  {
+    src: "./img/hongkong/hongkong_slide_001.jpg",
+    alt: "香港",
+    link: "./starseekers_hongkong.html",
+  },
   { src: "./img/taiwan_v.jpg", alt: "台灣", link: "./starseekers_taiwan.html" },
 ];
 
